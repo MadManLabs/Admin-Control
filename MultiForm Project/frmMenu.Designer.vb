486060a61626cbff0bd2ec95579880a4afdd6ae8
cease <cease@TECHBYTES>
2016-06-06 21:50:52 -0400

    Add project files.

---
+++ MultiForm Project/frmMenu.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMenu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMenu))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.cmbList = New System.Windows.Forms.ComboBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.btnCMD = New System.Windows.Forms.Button()
+        Me.btnMSTSC = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(416, 165)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Exit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-5, -3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(202, 205)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'cmbList
+        '
+        Me.cmbList.FormattingEnabled = True
+        Me.cmbList.Items.AddRange(New Object() {"Accessibility Options", "Add/Remove Programs", "Date/Time Properties", "Display Properties", "Internet Properties", "Mouse Properties", "Network Properties", "Printers Folder", "Sound Properties", "System Properties"})
+        Me.cmbList.Location = New System.Drawing.Point(258, 54)
+        Me.cmbList.Name = "cmbList"
+        Me.cmbList.Size = New System.Drawing.Size(187, 21)
+        Me.cmbList.TabIndex = 4
+        Me.cmbList.Text = "Select Control Panel Options"
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(231, 9)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(238, 31)
+        Me.lblTitle.TabIndex = 5
+        Me.lblTitle.Text = "CIS Admin Control"
+        Me.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'btnCMD
+        '
+        Me.btnCMD.Location = New System.Drawing.Point(258, 122)
+        Me.btnCMD.Name = "btnCMD"
+        Me.btnCMD.Size = New System.Drawing.Size(75, 23)
+        Me.btnCMD.TabIndex = 6
+        Me.btnCMD.Text = "CMD"
+        Me.btnCMD.UseVisualStyleBackColor = True
+        '
+        'btnMSTSC
+        '
+        Me.btnMSTSC.Location = New System.Drawing.Point(384, 122)
+        Me.btnMSTSC.Name = "btnMSTSC"
+        Me.btnMSTSC.Size = New System.Drawing.Size(75, 23)
+        Me.btnMSTSC.TabIndex = 7
+        Me.btnMSTSC.Text = "MSTSC"
+        Me.btnMSTSC.UseVisualStyleBackColor = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(272, 81)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(46, 35)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 8
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(399, 81)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(46, 35)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox3.TabIndex = 8
+        Me.PictureBox3.TabStop = False
+        '
+        'frmMenu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.ClientSize = New System.Drawing.Size(501, 200)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.btnMSTSC)
+        Me.Controls.Add(Me.btnCMD)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.cmbList)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmMenu"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "frmMenu"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents cmbList As ComboBox
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents btnCMD As Button
+    Friend WithEvents btnMSTSC As Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+End Class
---
+++ MultiForm Project/frmMenu.vb
@@ -0,0 +1,71 @@
+﻿Public Class frmMenu
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub cmbList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbList.SelectedIndexChanged
+        If cmbList.SelectedItem = "Display Properties" Then
+            Process.Start("C:\Windows\System32\cmd.exe", String.Format("/c  control desk.cpl"))
+
+        ElseIf cmbList.SelectedItem = "Accessibility Options" Then
+            Process.Start("C:\Windows\System32\cmd.exe", String.Format("/c  control access.cpl"))
+
+        ElseIf cmbList.SelectedItem = "Add/Remove Programs" Then
+            Process.Start("C:\Windows\System32\cmd.exe", String.Format("/c  control appwiz.cpl"))
+
+        ElseIf cmbList.SelectedItem = "Date/Time Properties" Then
+            Process.Start("C:\Windows\System32\cmd.exe", String.Format("/c  control timedate.cpl"))
+
+        ElseIf cmbList.SelectedItem = "Internet Properties" Then
+            Process.Start("C:\Windows\System32\cmd.exe", String.Format("/c  control inetcpl.cpl"))
+
+        ElseIf cmbList.SelectedItem = "Mouse Properties" Then
+            Process.Start("C:\Windows\System32\cmd.exe", String.Format("/c  control main.cpl"))
+
+        ElseIf cmbList.SelectedItem = "Network Properties" Then
+            Process.Start("C:\Windows\System32\cmd.exe", String.Format("/c  control Ncpa.cpl"))
+
+        ElseIf cmbList.SelectedItem = "Sound Properties" Then
+            Process.Start("C:\Windows\System32\cmd.exe", String.Format("/c  control mmsys.cpl sounds"))
+
+        ElseIf cmbList.SelectedItem = "System Properties" Then
+            Process.Start("C:\Windows\System32\cmd.exe", String.Format("/c  control sysdm.cpl"))
+
+
+        ElseIf cmbList.SelectedItem = "Printers Folder" Then
+            Process.Start("C:\Windows\System32\cmd.exe", String.Format("/c  control printers"))
+
+        End If
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+    End Sub
+
+    Private Sub btnCMD_Click(sender As Object, e As EventArgs) Handles btnCMD.Click
+        Process.Start("CMD.exe", String.Format("/k cd c:\"))
+    End Sub
+
+    Private Sub btnMSTSC_Click(sender As Object, e As EventArgs) Handles btnMSTSC.Click
+        Process.Start("mstsc.exe")
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+End Class
